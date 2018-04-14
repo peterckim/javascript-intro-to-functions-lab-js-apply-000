25717ffe679ade3e0df171cacbcb5c892f5258c0
Peter Kim <peterckim96@gmail.com>
2018-04-14 19:40:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === 'I love you, Grandma') {
-    return 
+    return 'I love you, too';
   }
+  
   if (string.toUpperCase() === string) {
     return 'YES INDEED!';
   } else if (string.toLowerCase() === string) {
