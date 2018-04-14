6ab910151fb4d31082c0c82adf95fe738fb7c123
Peter Kim <peterckim96@gmail.com>
2018-04-14 19:35:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  
+  return console.log(string.toUpperCase());
 }
 
 function logWhisper(string) {
-  
+  return console.log(string.toUpperCase());
 }
